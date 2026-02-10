179e71558833da972e14cea5e61dc86f7e5a3464
douaetoumiat <douaetoumiat5@gmail.com>
2026-02-10 15:44:43 +0100

    second done

--- P06/S03/dna_count.py
+++ P06/S03/dna_count.py
@@ -1,14 +1,14 @@
 
 def letters(letter,seq):
     count = 0
-    for i in range(0,len(seq)+1):
-        if i == letter :
+    for i in range(0,len(seq)):
+        if seq[i] == letter :
             count += 1
     return count
 
-
-print("The lenght of the sequence is:", len("CATGTAGACTAG"))
-print("A:",letters("A","CATGTAGACTAG"))
-print("C:",letters("C","CATGTAGACTAG"))
-print("G:",letters("G","CATGTAGACTAG"))
-print("T:",letters("T","CATGTAGACTAG"))
+sec = "CATGTAGACTAG"
+print("The lenght of the sequence is:", len(sec))
+print("A:",letters("A",sec))
+print("C:",letters("C",sec))
+print("G:",letters("G",sec))
+print("T:",letters("T",sec))
