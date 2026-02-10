3c75cb71e8f2b2484bcd64c1f7c4c676b92ceed9
douaetoumiat <douaetoumiat5@gmail.com>
2026-02-10 16:19:29 +0100

    done

--- S03/dna_count_file.py
+++ S03/dna_count_file.py
@@ -1,16 +1,17 @@
 
-def count_bases(seq):
+def count_bases(seq1):
     count_total = 0
-    count_bases = 0
+    count_bases1 = 0
     bases =[]
-    for i in range(0,len(seq)):
-        num = len(seq[i])
+    for i in range(0,len(seq1)):
+        num = len(seq1[i])
         count_total += num
-        for l in range(0,len(seq[i])):
-            if seq[i][l] not in bases :
-                count_bases += 1
-                bases.append(seq[i][l])
-    return count_total ,count_bases
+        seq1[i] = seq1[i].strip()
+        for l in range(0,len(seq1[i])):
+            if seq1[i][l] not in bases :
+                count_bases1 += 1
+                bases.append(seq1[i][l])
+    return count_total ,count_bases1
 
 
 
@@ -19,3 +20,6 @@ seq = file.readlines()
 answer = count_bases(seq)
 print("the total bases are:",answer[0])
 print("the different bases are:",answer[1])
+
+
+
