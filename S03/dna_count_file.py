38f8b5134b223aa8171d0b753db878f5fdfced28
douaetoumiat <douaetoumiat5@gmail.com>
2026-02-10 16:01:00 +0100

    done

--- S03/dna_count_file.py
+++ S03/dna_count_file.py
@@ -1,13 +1,18 @@
-file  = open("/home/alumnos/douae/PycharmProjects/pne-studentslab/S03/dna.txt")
+
 def count_bases(seq):
     count_total = 0
     count_bases = 0
     bases =[]
     for i in range(0,len(seq)):
-         num = len(seq[i])
-         count_total += num
+        num = len(seq[i])
+        count_total += num
         for l in range(0,len(seq[i])):
-             if seq[i][l] not in bases :
-             count_bases += 1
-             bases.append[seq[i][l]]
+            if seq[i][l] not in bases :
+                count_bases += 1
+                bases.append(seq[i][l])
 
+
+
+file  = open("/home/alumnos/douae/PycharmProjects/pne-studentslab/S03/dna.txt")
+seq = file.readlines()
+print(count_bases(seq))
