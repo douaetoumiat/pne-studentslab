8211906763baeef2e4b3ca31005a78717d6ebb64
douaetoumiat <douaetoumiat5@gmail.com>
2026-02-10 15:37:20 +0100

    Bug found

--- P06/S03/dna_count.py
+++ P06/S03/dna_count.py
@@ -1,6 +1,14 @@
 
 def letters(letter,seq):
-    for seq in
+    count = 0
+    for i in range(0,len(seq)+1):
+        if i == letter :
+            count += 1
+    return count
 
-def counter(seq) :
-    length = len(seq.strip())
+
+print("The lenght of the sequence is:", len("CATGTAGACTAG"))
+print("A:",letters("A","CATGTAGACTAG"))
+print("C:",letters("C","CATGTAGACTAG"))
+print("G:",letters("G","CATGTAGACTAG"))
+print("T:",letters("T","CATGTAGACTAG"))
