6e876e20a8f0386fce797aa8cd40a95f9b6452ba
douaetoumiat <douaetoumiat5@gmail.com>
2026-02-18 17:37:32 +0100

    done

---
+++ p00/seq0.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+def seq_ping():
+    print("Ok")
+
+def seq_read_fasta(filename):
+    file_contents = Path(filename).read_text()
+    line = file_contents.split("\n")
+    delimeter_space = " "
+    sequences = []
+    for i in range(0, len(line)):
+        if not (line[i]).startswith(">"):
+            sequences.append(line[i])
+    sequence = delimeter_space.join(sequences)
+
+    return (sequence.replace(" ", ""))
+
+def seq_len(seq):
+    return len(seq)
+
+def seq_count_base(seq, base):
+    base_count = 0
+    for i in range(0,len(seq)):
+        if seq[i] == base :
+            base_count += 1
+
+    return base_count
+
+def seq_count(seq):
+    s = "ACGT"
+    h = {}
+    for m in s:
+      count = 0
+      for n in seq:
+         if n == m :
+           count += 1
+      h[m] = count
+    for key, value in h.items():
+        print(key, value)
+
+
+print(seq_count_base(seq_read_fasta("/home/alumnos/douae/PycharmProjects/pne-studentslab/S04/text files/Homo_sapiens_ADA_sequence.txt"),"A"))
+print(seq_count(seq_read_fasta("/home/alumnos/douae/PycharmProjects/pne-studentslab/S04/text files/Homo_sapiens_ADA_sequence.txt")))
