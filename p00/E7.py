2551d85004d2c6fe795b5eae79173707a277c773
douaetoumiat <douaetoumiat5@gmail.com>
2026-02-18 18:46:52 +0100

    done

--- p00/E7.py
+++ p00/E7.py
@@ -3,5 +3,5 @@ from seq0 import *
 seq = seq_read_fasta("/home/alumnos/douae/PycharmProjects/pne-studentslab/S04/text files/Danio_rerio_U5_sequence.txt")
 short_seq = seq[0:19]
 print("GENE U5:")
-print("original:",short_seq)
-print("reversed:",seq_reverse(short_seq))
+print("frag:",short_seq)
+print("comp:",seq_complement(short_seq))
---
+++ p00/E8.py
@@ -0,0 +1,10 @@
+from seq0 import *
+genes = ["Danio_rerio_U5_sequence","Homo_sapiens_ADA_sequence","Homo_sapiens_FRAT1_sequence","Homo_sapiens_FXN_sequence"]
+bases = ["A","T","G","C"]
+result = {}
+for i in range(0,len(genes)):
+  result[genes[i]] = {}
+  for m in range(0,len(bases)):
+      result[genes[i]][bases[m]] = seq_count_base((seq_read_fasta("/home/alumnos/douae/PycharmProjects/pne-studentslab/S04/text files/"+ genes[i] + ".txt")),bases[m])
+
+print(result)
