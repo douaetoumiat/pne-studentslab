35b0f1355bc5f3934cbaac77ad3b56c01b29d142
douaetoumiat <douaetoumiat5@gmail.com>
2026-02-25 18:41:02 +0100

    done

---
+++ P01/e4.py
@@ -0,0 +1,8 @@
+from Seq1 import Seq
+print("-----| Practice 1, Exercise 5 |------")
+s1 = Seq()
+s2 = Seq("ACTGA")
+s3 = Seq("aahjki")
+print("sequence 1;" , s1,s1.count())
+print("sequence 2;", s2,s2.count())
+print("sequence 3;" , s3, s3.count())
