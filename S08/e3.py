fba2cbd6b9972be2d8db0843679bd07bb3b75dd6
douaetoumiat <douaetoumiat5@gmail.com>
2026-03-03 16:40:04 +0100

    done

---
+++ S08/e3.py
@@ -0,0 +1,22 @@
+import socket
+
+# Configure the Server's IP and PORT
+PORT = 8081
+IP = "192.168.124.179" # it depends on the machine the server is running
+
+while True:
+       message = input("Write a message")
+        clientsocket.send(send_bytes)
+        clientsocket.close()
+
+
+while True:
+  # -- Ask the user for the message
+
+  # -- Create the socket
+
+  # -- Establish the connection to the Server
+
+  # -- Send the user message
+
+  # -- Close the socket
